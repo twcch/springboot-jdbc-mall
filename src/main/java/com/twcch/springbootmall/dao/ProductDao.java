04b2718dadc2448af77598a4682614c037eb84bf
twcch <twcch1218@gmail.com>
2023-03-14 21:15:46 +0800

    部份程式碼優化

--- src/main/java/com/twcch/springbootmall/dao/ProductDao.java
+++ src/main/java/com/twcch/springbootmall/dao/ProductDao.java
@@ -7,14 +7,14 @@ import java.util.List;
 
 public interface ProductDao {
 
-    public Product getProductById(int productId);
+    Product getProductById(int productId);
 
-    public List<Product> getProducts();
+    List<Product> getProducts();
 
-    public Integer createProduct(ProductRequest productRequest);
+    Integer createProduct(ProductRequest productRequest);
 
-    public void updateProduct(int productId, ProductRequest productRequest);
+    void updateProduct(int productId, ProductRequest productRequest);
 
-    public void deleteProduct(int productId);
+    void deleteProduct(int productId);
 
 }
