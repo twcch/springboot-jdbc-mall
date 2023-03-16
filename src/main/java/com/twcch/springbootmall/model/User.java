756735be85bcc5fa58203d4e8fa091f79a876741
twcch <twcch1218@gmail.com>
2023-03-16 20:57:12 +0800

    部份程式碼優化

--- src/main/java/com/twcch/springbootmall/model/User.java
+++ src/main/java/com/twcch/springbootmall/model/User.java
@@ -1,7 +1,6 @@
 package com.twcch.springbootmall.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonKey;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Date;
