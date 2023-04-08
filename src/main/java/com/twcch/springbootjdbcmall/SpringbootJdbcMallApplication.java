586ae3726ea77d1782ff8899870d5ae489081c07
twcch <twcch1218@gmail.com>
2023-04-08 14:42:21 +0800

    update

--- src/main/java/com/twcch/springbootjdbcmall/SpringbootJdbcMallApplication.java
+++ src/main/java/com/twcch/springbootjdbcmall/SpringbootJdbcMallApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootJdbcMallApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootJdbcMallApplication.class, args);//
+        SpringApplication.run(SpringbootJdbcMallApplication.class, args);
     }
 
 }
