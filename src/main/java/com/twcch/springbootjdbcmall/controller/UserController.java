be298f4e2640f2a07a22ed02a20c2e73f13e554a
twcch <twcch1218@gmail.com>
2023-10-02 22:27:40 +0800

    帳號功能 - 登入

--- src/main/java/com/twcch/springbootjdbcmall/controller/UserController.java
+++ src/main/java/com/twcch/springbootjdbcmall/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.twcch.springbootjdbcmall.controller;
 
+import com.twcch.springbootjdbcmall.dto.UserLoginRequest;
 import com.twcch.springbootjdbcmall.dto.UserRegisterRequest;
 import com.twcch.springbootjdbcmall.model.User;
 import com.twcch.springbootjdbcmall.service.UserService;
@@ -25,6 +26,15 @@ public class UserController {
         User user = userService.getUserById(userId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
+
+        User user = userService.login(userLoginRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
 
     }
 
--- src/main/java/com/twcch/springbootjdbcmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/twcch/springbootjdbcmall/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.twcch.springbootjdbcmall.service.impl;
 
 import com.twcch.springbootjdbcmall.dao.UserDao;
+import com.twcch.springbootjdbcmall.dto.UserLoginRequest;
 import com.twcch.springbootjdbcmall.dto.UserRegisterRequest;
 import com.twcch.springbootjdbcmall.model.User;
 import com.twcch.springbootjdbcmall.service.UserService;
@@ -33,6 +34,25 @@ public class UserServiceImpl implements UserService {
 
         // 創建帳號
         return userDao.createUser(userRegisterRequest);
+
+    }
+
+    @Override
+    public User login(UserLoginRequest userLoginRequest) {
+
+        User user = userDao.getUserByEmail(userLoginRequest.getEmail());
+
+        if (user == null) {
+            log.warn("該 email {} 尚未註冊!", userLoginRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!user.getPassword().equals(userLoginRequest.getPassword())) {
+            log.warn("email {} 的密碼不正確!", userLoginRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        return user;
 
     }
 
