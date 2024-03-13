8548c0fd0a7a7dfa43f3201e94208a4b7c9763b7
twcch <twcch1218@gmail.com>
2024-03-13 20:04:38 +0800

    商品功能 - 查詢商品

--- src/main/java/com/twcch/springbootjdbcmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/twcch/springbootjdbcmall/dao/impl/ProductDaoImpl.java
@@ -23,7 +23,7 @@ public class ProductDaoImpl implements ProductDao {
 
 		String sql = "SELECT product_id, product_name, category, image_url, price, "
 				+ "stock, description, created_date, last_modified_date "
-				+ "FROM product WHERE product_id = :productId;";
+				+ "FROM products WHERE product_id = :productId;";
 
 		Map<String, Object> map = new HashMap<>();
 		map.put("productId", productId);
